9ad08124c56a6a623c236897e2bb89d67952cac7
Codeshaver <hubbard.bryn@gmail.com>
2019-09-05 21:18:51 -0700

    added days of the week and fixed get.day and get.date mix up

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 function startTime() {
   var today = new Date();
   var c = today.getMonth()+1; 
-  var d = today.getDay();
+  var w = today.getDay();
+  var d = today.getDate();
   var y = today.getYear()-100; 
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
+  if (w==0) {
+w = "Sunday" 
+}
+  if (w==1) {
+w = "Monday" 
+}
+  if (w==2) {
+w = "Tuesday" 
+}
+  if (w==3) {
+w = "Wednesday" 
+}
+  if (w==4) {
+w = "Thursday" 
+}
+  if (w==5) {
+w = "Friday" 
+}
+  if (w==6) {
+w = "Saturday" 
+}
 //don't understand the script immediately below. Don't understand why 'txt' is a div etc.
   document.getElementById('txt').innerHTML =
-  c + "/" + d + "/" + y + "@" + h + ":" + m + ":" + s;
+ w + " " + c + "/" + d + "/" + y + "@" + h + ":" + m + ":" + s;
 //I know that this is why my second's are updating but I don't know why. Where is the variable t
   var t = setTimeout(startTime, 500);
 }
