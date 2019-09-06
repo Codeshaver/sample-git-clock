84f28242943d8ce9d2cc3468e8bc9381a84119e0
Codeshaver <hubbard.bryn@gmail.com>
2019-09-06 11:43:41 -0700

    added some css, months, and 12/24 hour  button. Need to make button operational

--- main.js
+++ main.js
@@ -3,40 +3,89 @@ function startTime() {
   var c = today.getMonth()+1; 
   var w = today.getDay();
   var d = today.getDate();
-  var y = today.getYear()-100; 
+  var y = today.getFullYear(); 
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
+  var a = today.getHours()-12;
+  var button = document.createElement('button');
   m = checkTime(m);
   s = checkTime(s);
+
+  if (c==1) {
+c = "January";
+}
+  if (c==2) {
+c = "February"; 
+}
+  if (c==3) {
+c = "March"; 
+}
+  if (c==4) {
+c = "April"; 
+}
+  if (c==5) {
+c = "May"; 
+}
+  if (c==6) {
+c = "June"; 
+}
+  if (c==7) {
+c = "July";
+}
+  if (c==8) {
+c = "August"; 
+}
+  if (c==9) {
+c = "September"; 
+}
+  if (c==10) {
+c = "October"; 
+}
+  if (c==11) {
+c = "November"; 
+}
+  if (c==12) {
+c = "December"; 
+}
   if (w==0) {
-w = "Sunday" 
+w = "Sunday"; 
 }
   if (w==1) {
-w = "Monday" 
+w = "Monday"; 
 }
   if (w==2) {
-w = "Tuesday" 
+w = "Tuesday"; 
 }
   if (w==3) {
-w = "Wednesday" 
+w = "Wednesday"; 
 }
   if (w==4) {
-w = "Thursday" 
+w = "Thursday"; 
 }
   if (w==5) {
-w = "Friday" 
+w = "Friday"; 
 }
   if (w==6) {
-w = "Saturday" 
+w = "Saturday"; 
 }
-//don't understand the script immediately below. Don't understand why 'txt' is a div etc.
+  if (w==0) {
+w = "Sunday"; 
+}
+
+function myFunction() {
+  ;
+}
+
   document.getElementById('txt').innerHTML =
- w + " " + c + "/" + d + "/" + y + "@" + h + ":" + m + ":" + s;
-//I know that this is why my second's are updating but I don't know why. Where is the variable t
-  var t = setTimeout(startTime, 500);
+ w + " " + c + "," + " " + d + "," + " " + y + "@" + h + ":" + m + ":" + s;
+
 }
+
+//is setInterval a function and if so why doesn't it need a function qualifier
+setInterval(startTime, 1000);
 function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
   return i;
 }
+startTime();
